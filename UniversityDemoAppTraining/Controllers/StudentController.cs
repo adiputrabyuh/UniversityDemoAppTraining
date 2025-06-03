674ccbb99863e102ea1e3a405d5d830969c9c5b8
ramaputra1 <adiputra@go.byuh.edu>
2025-06-03 09:36:07 -1000

    POST or Add a new student finsihed.

--- UniversityDemoAppTraining/Controllers/StudentController.cs
+++ UniversityDemoAppTraining/Controllers/StudentController.cs
@@ -5,16 +5,20 @@ namespace UniversityDemoAppTraining.Controllers
 
 {
     [ApiController]
-    [Route("Controller")]
+    [Route("api/[controller]")]
     public class StudentController : ControllerBase
     {
         private readonly DbMySQLService _DbMySQLSevice;
+
         private readonly StudentService _studentService;
+
         public StudentController(DbMySQLService DbMySQLService, StudentService studentService)
         {
             _DbMySQLSevice = DbMySQLService;
             _studentService = studentService;
         }
+
+        // GET: api/Student/GetAll
         [HttpGet]
         [Route("GetAll")]
         public IActionResult GetAll()
@@ -22,5 +26,34 @@ namespace UniversityDemoAppTraining.Controllers
             var students = _studentService.GetStudentsAll();
             return Ok(students);
         }
+
+        // GET: api/Student/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetStudentById(int id)
+        {
+            var student = _studentService.GetByID(id);
+            if (student == null)
+                return Ok();
+            return Ok(student);
+        }
+
+        // POST: api/Student/AddStudent
+        [HttpPost]
+        [Route("AddStudent")]
+        public IActionResult AddStudent([FromBody] Student student)
+        {
+            try
+            {
+                student.id = 0;
+                _studentService.AddStudent(student);
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+
     }
 }
--- UniversityDemoAppTraining/Services/StudentService.cs
+++ UniversityDemoAppTraining/Services/StudentService.cs
@@ -7,6 +7,7 @@ namespace UniversityDemoAppTraining.Services
     public class StudentService : DbMySQLService
     {
 
+        // Get All the Students
         public List<Student> GetStudentsAll()
         {
             List<Student> students = new List<Student>();
@@ -32,6 +33,69 @@ namespace UniversityDemoAppTraining.Services
             }
             return students;
 
+        }
+
+        // Get the student by Id
+        public Student? GetByID(int id)
+        {
+            using (MySqlConnection connection = GetOpenMySqlConnection())
+            {
+                string studentSql = "SELECT id, first_name, last_name, email, enrollment_date FROM student WHERE id = @id;";
+                MySqlCommand cmd = new MySqlCommand(studentSql, connection);
+                cmd.Parameters.AddWithValue("@id", id);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Student()
+                        {
+                            id = reader.GetInt32("id"),
+                            first_name = reader.GetString("first_name"),
+                            last_name = reader.GetString("last_name"),
+                            email = reader.GetString("email"),
+                            enrollment_date = reader.GetDateTime("enrollment_date")
+                        };
+                    }
+                }
+            }
+            return null;
+        }
+
+        //Add a new student
+        public void AddStudent(Student student)
+        {
+            string studentSql = "INSERT INTO student (first_name, last_name, email) VALUES (@first_name, @last_name, @email);";
+
+            using (MySqlConnection connection = GetOpenMySqlConnection())
+            {
+                MySqlCommand studentCmd = new MySqlCommand(studentSql, connection);
+                studentCmd.Parameters.AddWithValue("@first_name", student.first_name);
+                studentCmd.Parameters.AddWithValue("@last_name", student.last_name);
+                studentCmd.Parameters.AddWithValue("@email", student.email);
+                //studentCmd.Parameters.AddWithValue("@enrollment_date", student.enrollment_date);
+
+                try
+                {
+                    // Execute the command to insert the student into the database
+                    int rowsAffected = studentCmd.ExecuteNonQuery();
+
+                    // Check if any rows were affected (this means the insert was successful)
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Student added successfully!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No student was added.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle any errors that occur during the query execution
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+
+            }
         }
     }
 }
